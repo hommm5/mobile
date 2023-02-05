8338620b8c7fc82e7aed43a39740040955a764c3
hommm5 <hommm5@gmail.com>
2023-02-05 17:24:58 +0200

    some files

---
+++ src/test/java/bg/softuni/mobile/MobileApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
