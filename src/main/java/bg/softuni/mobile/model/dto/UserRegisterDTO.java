1eadaa2c0ba3658ebdf32424fdad3403ce46b0fd
hommm5 <hommm5@gmail.com>
2023-02-13 23:45:44 +0200

    some files

--- src/main/java/bg/softuni/mobile/model/dto/UserRegisterDTO.java
+++ src/main/java/bg/softuni/mobile/model/dto/UserRegisterDTO.java
@@ -1,9 +1,20 @@
 package bg.softuni.mobile.model.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class UserRegisterDTO {
+    @NotEmpty
+    @Size(min = 2, max = 20)
     private String firstName;
+    @NotEmpty
+    @Size(min = 2, max = 20)
     private String lastName;
+    @Email
     private String email;
+    @NotEmpty
+    @Size(min = 5)
     private String password;
     private String confirmPassword;
 
